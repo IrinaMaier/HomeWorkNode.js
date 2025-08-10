717ccf2454fe101c82a2ee5097892ac432dbe069
Maier Irina <maieririna@mail.de>
2025-08-10 21:53:33 +0200

    added my home work number 8

---
+++ homeWork8node/migrations/20250809203347-create-book.cjs
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Books', {
+      id: { type: Sequelize.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true },
+      title:  { type: Sequelize.STRING,  allowNull: false },
+      author: { type: Sequelize.STRING,  allowNull: false },
+      year:   { type: Sequelize.INTEGER, allowNull: false }
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('Books');
+  }
+};
+
+
